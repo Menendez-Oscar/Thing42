3d325cc781955129b05359394b1f5ff911b6011b
oscar <ormg1909@gmail.com>
2014-09-06 13:43:40 -0600

    documented Thing42 to match interface given in specifications

--- src/Thing42.java
+++ src/Thing42.java
@@ -18,10 +18,25 @@ import java.util.Map;
 public class Thing42<K, D> implements Thing42orNull<K, D> {
 
 	public static final String NPE_MESSAGE = "Argument cannot be null";
+	/**
+	 * Generic mutable attribute data
+	 */
 	private D data;
+	/**
+	 * Generic immutable generic key
+	 */
 	private final K key;
+	/**
+	 * ordered collection of Thing42 objects pool.
+	 */
 	private final long level;
+	/**
+	 * ordered collection of Thing42 objects pool.
+	 */
 	private ArrayList<Thing42orNull<K, D>> pool;
+	/**
+	 * unordered collection of Thing42 objects peers
+	 */
 	private Map<K, LinkedList<Thing42orNull<K, D>>> peers;
 
 	/**
@@ -40,42 +55,45 @@ public class Thing42<K, D> implements Thing42orNull<K, D> {
 	}
 
 	/**
-	 * Gets the data
+	 * Access the data of this Thing42.
 	 * 
-	 * @return the data
+	 * @return The data of this object
 	 */
-        @Override
+	@Override
 	public D getData() {
 		return this.data;
 	}
 
 	/**
-	 * Gets the key
+	 * Access the key of this Thing42.
 	 * 
-	 * @return the key
+	 * @return the key of this object
 	 */
-        @Override
+	@Override
 	public K getKey() {
 		return this.key;
 	}
 
 	/**
-	 * Sets the data
+	 * Modify the data of this Thing42.
 	 * 
 	 * @param newData
-	 *            the new data
+	 *            the updated data for this object
 	 */
-        @Override
+	@Override
 	public void setData(D newData) {
 		this.data = newData;
 	}
 
 	/**
-	 * Adds peer to {@link Thing42orNull} list. If
+	 * Add a peer to this object.
 	 * 
 	 * @param newPeer
+	 *            Thing42orNull object to be added to peers set
+	 * @throws NullPointerException
+	 *             - if the specified peer is null
 	 */
-        @Override
+	@Override
 	public void addPeer(Thing42orNull<K, D> newPeer) {
 		nullCheck(newPeer);
 		K key = newPeer.getKey();
@@ -86,34 +104,38 @@ public class Thing42<K, D> implements Thing42orNull<K, D> {
 	}
 
 	/**
-	 * Appends new {@link Thing42orNull} to member pool.
+	 * Append a member to the pool of this object.
 	 * 
 	 * @param newMember
+	 *            Thing42 object to be appended to pool list
+	 * @throws NullPointerException
+	 *             - if the specified item is null
 	 */
-        @Override
+	@Override
 	public void appendToPool(Thing42orNull<K, D> newMember) {
 		nullCheck(newMember);
 		this.pool.add(newMember);
 	}
 
 	/**
-	 * Gets level
+	 * Access the level of this Thing42.
 	 * 
-	 * @return level
+	 * @return the level of this object
 	 */
-        @Override
+	@Override
 	public long getLevel() {
 		return this.level;
 	}
 
 	/**
-	 * Gets {@link Thing42orNull} peer by key
+	 * Access a peer matching the specified key.
 	 * 
-	 * @see Thing42orNull
 	 * @param key
-	 * @return {@link Thing42orNull} or null if not found
+	 *            the search key
+	 * @return any peer known by this object that matches the given key; null if
+	 *         no match
 	 */
-        @Override
+	@Override
 	public Thing42orNull<K, D> getOnePeer(K key) {
 		Thing42orNull<K, D> one = null;
 		List<Thing42orNull<K, D>> matchingKeys = this.peers.get(key);
@@ -124,13 +146,12 @@ public class Thing42<K, D> implements Thing42orNull<K, D> {
 	}
 
 	/**
-	 * Gets peer or peers as {@link java.util.Collection}
+	 * Access all peers.
 	 * 
-	 * @return {@link java.util.Collection} of {@link Open Declaration
-	 *         java.util.LinkedList.LinkedList} containing all peer
-	 *         {@link Thing42orNull}
+	 * @return all peers known by this object; if no peers then returns a
+	 *         collection with size() == 0.
 	 */
-        @Override
+	@Override
 	public Collection<Thing42orNull<K, D>> getPeersAsCollection() {
 		List<Thing42orNull<K, D>> retValue = new LinkedList<Thing42orNull<K, D>>();
 		Collection<LinkedList<Thing42orNull<K, D>>> lists = this.peers.values();
@@ -142,49 +163,56 @@ public class Thing42<K, D> implements Thing42orNull<K, D> {
 	}
 
 	/**
-	 * Gets peer or peers as {@link java.util.Collection} that matches the key.
+	 * Access all peers matching the specified key.
 	 * 
 	 * @param key
-	 * @return {@link java.util.Collection} of {@link Open Declaration
-	 *         java.util.LinkedList.LinkedList} containing all peer
-	 *         {@link Thing42orNull} that match the key.
+	 *            the search key
+	 * @return all peers known by this object that match the given key; if no
+	 *         peer matches then returns a collection with size() == 0.
 	 */
-        @Override
+	@Override
 	public Collection<Thing42orNull<K, D>> getPeersAsCollection(Object key) {
 		Collection<Thing42orNull<K, D>> mpeers = this.peers.get(key);
 		return mpeers == null ? new LinkedList<Thing42orNull<K, D>>() : mpeers;
 	}
 
 	/**
+	 * Access all members of the pool.
 	 * 
-	 * @return
+	 * @return all members of the pool known by this object; if no members then
+	 *         returns a list with size() == 0.
 	 */
-        @Override
+	@Override
 	public List<Thing42orNull<K, D>> getPoolAsList() {
 		return this.pool;
 	}
 
 	/**
-	 * Removes {@link Thing42orNull} from pool
+	 * Remove a single instance of the specified object from this object's pool.
 	 * 
 	 * @param member
-	 *            {@link Thing42orNull} to be removed
-	 * @return {@code true} if found and removed, otherwise {@code false}
+	 *            the member to be removed from the pool
+	 * @return true if a pool member was removed as a result of this call
+	 * @throws NullPointerException
+	 *             - if the specified parameter is null
 	 */
-        @Override
+	@Override
 	public boolean removeFromPool(Thing42orNull<K, D> member) {
 		nullCheck(member);
 		return this.pool.remove(member);
 	}
 
 	/**
-	 * Removes {@link Thing42orNull} peer
+	 * Remove a single instance of the specified peer from this object.
 	 * 
 	 * @param peer
-	 *            {@link Thing42orNull} to be removed.
-	 * @return {@code true} if found and removed, otherwise {@code false}
+	 *            - the peer to be removed
+	 * @return true if a peer was removed as a result of this call
+	 * @throws NullPointerException
+	 *             - if the specified peer is null
 	 */
-        @Override
+
+	@Override
 	public boolean removePeer(Thing42orNull<K, D> peer) {
 		nullCheck(peer);
 		return this.peers.get(peer.getKey()).remove(peer);
